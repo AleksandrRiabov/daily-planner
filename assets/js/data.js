f56595077cf864c59b8e7a22788be4a15fcce0ee
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-20 21:43:17 +0000

    Self removing notification added

---
+++ assets/js/data.js
@@ -0,0 +1,12 @@
+export const workingHours = [
+  {time: '9am', message: ''},
+  {time: '10am', message: ''},
+  {time: '11am', message: ''},
+  {time: '12pm', message: ''},
+  {time: '1pm', message: 'Start to work'},
+  {time: '2pm', message: ''},
+  {time: '3pm', message: ''},
+  {time: '4pm', message: 'Finish working'},
+  {time: '5pm', message: ''},
+  {time: '6pm', message: ''},
+]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
+import { workingHours } from "./data.js";
+
+
 $(function () {
+  //Get current day for jumbotron
   $('#currentDay').text(moment().format('dddd, MMMM, Do'));
 
+  //Check if hours exist in localstorage if not use initial array from data file
+  let hours = JSON.parse(localStorage.getItem('hours')) || workingHours;
 
+  //Append timeBlock ellement wrapper to the page
+  const timeBlock = $('<div>').addClass('time-block');
 
-  function createRow(hour) {
-    const { time, text } = hour;
-
+  //Function to cretae new row/hour info
+  function createHourRow(hour, index) {
+    const { time, message } = hour;
+    //asign appropriate class deppending on time.
     const textareaClass =
       moment(time, 'h').format('h') < moment().format('h') ?
         'past' :
@@ -13,21 +22,52 @@ $(function () {
           'future' :
           'present'
 
-    return row = `
-        <div class="row">
-          <div class="hour col-sm-1">${time}</div>
-          <textarea class='${textareaClass} col-sm-10'>${text}</textarea>
-          <button class="saveBtn col-sm-1">Save</button>
+    return `
+        <div  data-hour=${time}  data-index=${index} class="row">
+          <div  class="hour col-sm-1">${time}</div>
+          <textarea class='${textareaClass} col-sm-10'>${message || ''}</textarea>
+          <button class="saveBtn col-sm-1">
+          <i class="fas fa-save"></i>
+          </button>
         </div>
       `
   }
 
 
-  $('.container').append(createRow({ time: '2pm', text: 'Hello, I have an appointment at this time!' }));
+  //Generate timeblock for each working hour
+  $.each(hours, function (index, hour) {
+    $(timeBlock).append(createHourRow(hour, index));
+  });
 
+  //On click save to localStorage and show message
+  $(timeBlock).on('click', '.saveBtn', function () {
+    const message = $(this).parent().children('textarea').val();
+    const time = $(this).parent().data('hour');
 
+    hours[$(this).parent().data('index')] = { message, time };
+    localStorage.setItem('hours', JSON.stringify(hours));
 
+    const notification = `
+    <h6 id="notification"> Appointment Added to the 
+    <span>localStorage</span>
+    <i class="fas fa-check"></i>
+    </h6>`
+    notify(timeBlock, notification)
+  })
+
+  //Add created timeblocks on the page
+  $('.container').append(timeBlock);
 });
 
 
-
+//Self removing notification
+let timeOutId;
+function notify(timeBlock, notification) {
+  clearTimeout(timeOutId);
+  $('#notification').remove();
+  $(timeBlock).prepend(notification);
+  timeOutId = setTimeout(() => {
+    $('#notification').remove();
+    console.log('removed')
+  }, 5000)
+}
