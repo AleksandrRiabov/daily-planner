6fe86985c2fba8b993831a2f1cf3d547e39b96b6
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-22 22:37:56 +0000

    Working hours changed to standard business hours in uk

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,12 +1,11 @@
 export const workingHours = [
-  {time: '9:am', message: ''},
-  {time: '10:am', message: ''},
-  {time: '11:am', message: ''},
-  {time: '12:pm', message: ''},
-  {time: '1:pm', message: 'Start homework'},
-  {time: '2:pm', message: ''},
-  {time: '3:pm', message: ''},
-  {time: '4:pm', message: 'Go for a run'},
-  {time: '5:pm', message: ''},
-  {time: '11:pm', message: ''},
+  { time: '9:am', message: '' },
+  { time: '10:am', message: '' },
+  { time: '11:am', message: '' },
+  { time: '12:pm', message: '' },
+  { time: '1:pm', message: 'Start homework' },
+  { time: '2:pm', message: '' },
+  { time: '3:pm', message: '' },
+  { time: '4:pm', message: 'Go for a run' },
+  { time: '5:pm', message: '' }
 ]
