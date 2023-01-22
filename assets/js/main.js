5ca387a2fbb681aa0e73593a7cdc7a68ab1fbb5d
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-22 01:39:29 +0000

    Changed the way classes assigned to textarea. Done refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,32 @@
 import { workingHours } from "./data.js";
-const today = moment();
 
 $(function () {
-  //Get current day for jumbotron
-  $("#currentDay").text(today.format("dddd, MMMM, Do"));
+  const currentTime = moment();
+  //Get current day for header / jumbotron
+  $("#currentDay").text(currentTime.format("dddd, MMMM, Do"));
 
   //Check if hours exist in localstorage if not use initial array from data file
   let hours = JSON.parse(localStorage.getItem("hours")) || workingHours;
 
   //Append timeBlock ellement wrapper to the page
-  const timeBlock = $("<div>").addClass("time-block");
+  const timeBlockEl = $("<div>").addClass("time-block");
 
-  //Function to cretae new row/hour info
+  //Function to cretae new row (single hour info)
   function createHourRow(hour, index) {
     const { time, message } = hour;
+    const rowTime = moment(time, "h:Aa");
 
-    const textareaClass = moment(
-      `${today.format("YYYY-MM-DD")} ${time}`
-    ).isBefore(`${today.format("YYYY-MM-DD HH")}`, "hour")
+    //Assign appropriate class based on current time
+    const textareaClass = currentTime.isAfter(rowTime, "hour")
       ? "past"
-      : moment(`${today.format("YYYY-MM-DD")} ${time}`).isAfter(
-          `${today.format("YYYY-MM-DD HH")}`,
-          "hour"
-        )
+      : currentTime.isBefore(rowTime, "hour")
       ? "future"
       : "present";
 
     return `
         <div data-hour=${time}  data-index=${index} class="row">
           <div  class="hour col-sm-12 col-md-1">${time.replace(":", "")}</div>
-          <textarea class='${textareaClass} col-sm-12 col-md-10'>${message || ""}</textarea>
+          <textarea class='${textareaClass} col-sm-12 col-md-10'>${message}</textarea>
           <button class="saveBtn col-sm-12 col-md-1">
              <i class="fas fa-save"></i>
           </button>
@@ -37,39 +34,39 @@ $(function () {
       `;
   }
 
-  //Generate timeblock for each working hour
+  //Generate timeblock for each working hour and append totimeBlock element
   $.each(hours, function (index, hour) {
-    $(timeBlock).append(createHourRow(hour, index));
+    $(timeBlockEl).append(createHourRow(hour, index));
   });
 
   //On click save to localStorage and show message
-  $(timeBlock).on("click", ".saveBtn", function () {
-    const message = $(this).parent().children("textarea").val();
+  $(timeBlockEl).on("click", ".saveBtn", function () {
+    const message = $(this).siblings("textarea").val();
     const time = $(this).parent().data("hour");
-
-    hours[$(this).parent().data("index")] = { message, time };
+    const currentRowIndex = $(this).parent().data("index");
+    //update hours array
+    hours[currentRowIndex] = { message, time };
     localStorage.setItem("hours", JSON.stringify(hours));
 
     const notification = `
-    <h6 id="notification"> Appointment Added to the 
-    <span>localStorage</span>
-    <i class="fas fa-check"></i>
-    </h6>`;
-    notify(timeBlock, notification);
+      <h6 id="notification"> Appointment Added to the 
+      <span>localStorage</span>
+      <i class="fas fa-check"></i>
+      </h6>`;
+    showNotification(timeBlockEl, notification);
   });
 
   //Add created timeblocks on the page
-  $(".container").append(timeBlock);
+  $(".container").append(timeBlockEl);
 });
 
 //Self removing notification
 let timeOutId;
-function notify(timeBlock, notification) {
+function showNotification(parrentEl, notification) {
   clearTimeout(timeOutId);
   $("#notification").remove();
-  $(timeBlock).prepend(notification);
+  $(parrentEl).prepend(notification);
   timeOutId = setTimeout(() => {
     $("#notification").remove();
-    console.log("removed");
   }, 5000);
 }
